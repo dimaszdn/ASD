a71275adb93aebb34796ae05b2a8bcd066e46935
Дмитрий Чепелев <dimaszdn@mail.ru>
2023-10-29 20:01:34 +0200

    add shell_sort and radix_sort

--- Sorting/inc/Sorting.h
+++ Sorting/inc/Sorting.h
@@ -74,14 +74,99 @@ namespace ch
 
         void shell_sort()
         {
-            
+            T tmp;
+            const int N = 5;
+            int i, j, dist;
+            int a[N];
+            a[0] = 9; a[1] = 5; a[2] = 3; a[3] = 2; a[4] = 1;
+            for (int k = 0; k < N; ++k)
+            {
+                dist = a[k];
+                for (i = dist; i < m_size; ++i)
+                {
+                    tmp = m_vec[i];
+                    for (j = i - dist; (j >= 0) && (m_vec[j] >= tmp); j = j - dist)
+                    {
+//                        std::cout << "[" << dist << "]" << " ";
+//                        this->print_vec1(j + dist, j);
+                        m_vec[j + dist] = m_vec[j];
+                        m_vec[j] = tmp;
+//                        std::cout <<"After: ";
+//                        this->print_vec1(j + dist, j);
+//                        std::cout << "\n";
+                    }
+                }
+            }
+        }
+
+        //**********************************
+        void counting_sort(int exp)
+        {
+            const int maxDigit = 10;
+            std::vector<int> count(maxDigit, 0);
+            std::vector<T> output(m_vec.capacity());
+
+            for (int i = 0; i < m_size; ++i)
+                count[(m_vec[i] / exp) % 10]++; //(512 / 100) % 10, exp = 100 in this instance
+
+            for (int i = 1; i < maxDigit; ++i)
+                count[i] += count[i - 1];
+
+            for (int i = m_size - 1; i >= 0; --i)
+            {
+                output[count[(m_vec[i] / exp) % 10] - 1] = m_vec[i];
+                count[(m_vec[i] / exp) % 10]--;
+            }
+
+            for (int i = 0; i < output.size(); ++i)
+                std::cout << output[i] << " ";
+            std::cout << "\n\n";
+
+            for (int i = 0; i < m_size; ++i)
+                m_vec[i] = output[i];
+        }
+
+        void radix_sort()
+        {
+            T max = this->getMax();
+            for (int exp = 1; max / exp > 0; exp = exp * 10)
+                this->counting_sort(exp);
+        }
+        //***********************************
+
+        void print_vec1(int x, int y)
+        {
+            for (int i = 0; i < m_vec.size(); ++i)
+            {
+                if (i == x || i == y)
+                {
+                    std::cout << "|" << m_vec[i] << "|" << " ";
+                }
+                else
+                    std::cout << m_vec[i] << " ";
+            }
+//            std::cout << "\n";
         }
 
         void print_vec()
         {
             for (int i = 0; i < m_vec.size(); ++i)
+            {
                 std::cout << m_vec[i] << " ";
+            }
             std::cout << "\n";
+        }
+
+    private:
+        T getMax()
+        {
+            T max = m_vec[0];
+            for (int i = 1; i < m_size; ++i)
+            {
+                if (m_vec[i] > max)
+                    max = m_vec[i];
+            }
+            return max;
         }
     };
 }
--- Sorting/main.cpp
+++ Sorting/main.cpp
@@ -9,18 +9,27 @@ int main()
     std::vector<int> v1 = { 10, -5, 16, 3, 0, 3, -100, 500, 0, -3, -1000};
     std::vector<int> v2 = {-50, 20, 100, 3, 4, 300, 6, 43, 21, 56}; //-50 3 4 6 20 21 43 56 100 300
     std::vector<int> v3 = {-50, 20, 100, 3};
-    std::vector<int> v0 = {};
-    auto S = new Sort<int>(v3);
+    std::vector<int> v0 = {9, 8, 5, 7, 5, 6, 5, 1};
+    std::vector<int> v4 = {32, 95, 90, 82, 24, 66, 35, 90, 75, 54, 40, 43, 90, 68};
+    std::vector<int> v5 = {654, 304, 100, 21, 5, 0};
+
+    auto S = new Sort<int>(v5);
+
+//    int a = -512;
+//    std::cout << (a / 100) % 10;
+
     try
     {
-        S->insertion_sort();
-        S->print_vec();
+//        S->print_vec();
+        S->radix_sort();
+//        S->print_vec();
     }
     catch (std::out_of_range& ex)
     {
         std::cout << ex.what() << "\n";
     }
 
+    delete S;
 
     return 0;
 }

5964f33ec3e4ac0864deb24774f90e39606bfccf
Дмитрий Чепелев <dimaszdn@mail.ru>
2023-11-25 16:01:27 +0200

    completed

--- Hash Table (chain method)/inc/HashTable.h
+++ Hash Table (chain method)/inc/HashTable.h
@@ -1,31 +1,35 @@
 #pragma once
 #include<string>
 #include<fstream>
+#include<list>
 #include<vector>
 
 class HashTable
 {
 private:
+    std::list<std::string>* table;
     std::ofstream fout;
-    std::vector<std::string> table;
+    size_t mCap;
 
 public:
-    explicit HashTable(size_t cap) : table(cap, " ")
+    explicit HashTable(size_t cap) : mCap(cap)
     {
-
+        table = new std::list<std::string>[mCap];
     }
 
     ~HashTable()
     {
-        fout.close();
+        delete[] table;
     }
+
+    [[nodiscard]] size_t getCap() const { return mCap; }
 
     int hashFunc(const std::string& word)
     {
         //H(s0, s1, ..., sk) = s0 * p^size-1 + s1 * p^size-2 + ... + s_k * p^0 (s-коды букв, p-произвольная const, size - длина слова)
         auto base = 13;
         int hash = 0;
-        auto mod = table.capacity();
+        auto mod = this->getCap();
         for (int i = 0; i < word.size(); ++i)
             hash += static_cast<int>(this->getCharCode(word[i]) * pow(base, word.size() - i - 1));
         return static_cast<int>(hash % mod);
@@ -34,16 +38,26 @@ public:
     void insert(const std::string& word)
     {
         int index = this->hashFunc(word);
-        while (table[index] != " " && table[index] != word)
-        {
-            if (index == table.size() - 1)
-                index = 0;
-            ++index;
-        }
-        table[index] = word;
-    }
 
-    [[nodiscard]] size_t getCap() const { return table.capacity(); }
+        if (index < 0)
+            throw std::out_of_range("Negative index!");
+
+        bool wordExists = true;
+        auto begin = table[index].begin();
+        while (begin != table[index].end())
+        {
+            if (*begin == word)
+            {
+                wordExists = false;
+                *begin = word;
+                break;
+            }
+            ++begin;
+        }
+
+        if (wordExists)
+            table[index].push_back(word);
+    }
 
     void outputFile()
     {
@@ -51,14 +65,18 @@ public:
         std::string tmp;
         const int maxSizeWord = 15;
         const int maxCountDigit = static_cast<int>(std::to_string(this->getCap()).size());
-        for (int i = 0; i < table.capacity(); ++i)
+        for (int i = 0; i < this->getCap(); ++i)
         {
             tmp = std::to_string(i);
             tmp.resize(maxCountDigit);
             fout << tmp << "|" << "\t";
-            tmp = table[i];
-            tmp.resize(maxSizeWord);
-            fout << tmp << "\n";
+            for (const auto& el : table[i])
+            {
+                tmp = el;
+                tmp.resize(maxSizeWord);
+                fout << tmp << "\t";
+            }
+            fout << "\n";
         }
     }
 
--- Hash Table (chain method)/main.cpp
+++ Hash Table (chain method)/main.cpp
@@ -1,6 +1,7 @@
 #include<HashTable.h>
+
+#include <iostream>
 #include<vector>
-#include<iostream>
 
 int main()
 {
